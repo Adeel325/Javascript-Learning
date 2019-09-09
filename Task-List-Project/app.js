d5a393cc6dbe22bf2cb01942150dd872f4156e1d
Muhammad Adeel <rana.adeel7273@gmail.com>
2019-09-09 23:27:24 +0500

    filter tasks implemented
    filter tasks implemented

--- Task-List-Project/app.js
+++ Task-List-Project/app.js
@@ -19,6 +19,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   // Clear Tasks
   clearBtn.addEventListener('click', clearTasks);
+  // Filter Tasks
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -63,4 +65,17 @@ function  clearTasks(){
   while(taskList.firstChild){
     taskList.removeChild(taskList.firstChild);
   }
+}
+//Filter Tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } 
+    else{
+      task.style.display = 'none';
+    }
+  });
 }
